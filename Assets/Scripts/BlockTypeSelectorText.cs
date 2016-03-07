ce6d16a6db891a02cb53ddd6690d8caae66894b1
ChessNeo <evonsheika@gmail.com>
2016-03-07 11:32:00 +0100

    Revert "Block selection done, starting camera movement"
    This reverts commit 510c72ffc089dd66e0146fe40591d2da71a60a29.

--- Assets/Scripts/BlockTypeSelectorText.cs
+++ Assets/Scripts/BlockTypeSelectorText.cs
@@ -8,7 +8,6 @@ public class BlockTypeSelectorText : MonoBehaviour
 
 	private int _index = 0;
 	private Text _text;
-	private GameObject _selected;
 
 	void Start()
 	{
@@ -18,28 +17,10 @@ public class BlockTypeSelectorText : MonoBehaviour
 		if (_text == null)
 			throw new UnityEngine.MissingComponentException("Missing Text component");
 		_text.text = _texts[_index];
-		transform.parent.gameObject.SetActive(false);
 	}
 
-	public void Select(GameObject newSelected)
+	public void Select()
 	{
-		_selected = newSelected;
-		Block blockScript = _selected.GetComponent<Block>() as Block;
-		if (blockScript == null)
-			throw new UnityEngine.MissingComponentException("Missing Block component");
-		if (blockScript.BlockType < 0)
-		{
-			_index = _texts.Length - 1;
-			_text.text = _texts[_index];
-		}
-		else
-			_text.text = _texts[blockScript.BlockType];
-	}
-
-	public void Unselect()
-	{
-		_selected = null;
-		transform.parent.gameObject.SetActive(false);
 	}
 
 	public void Previous()
--- Assets/Scripts/EditorCameraMovement.cs
+++ Assets/Scripts/EditorCameraMovement.cs
@@ -1,31 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
 public class EditorCameraMovement : MonoBehaviour
 {
-	[SerializeField] private Text _heightText;
-	[SerializeField] private string _baseText;
-
-	private float _heightOffset;
-
-	void Start()
+	void Update()
 	{
-		_heightOffset = transform.position.y;
-		ChangeHeightText();
-	}
-
-	public void ChangeHeight(float n)
-	{
-		Vector3 newPosition = transform.position;
-
-		newPosition.y += n;
-		transform.position = newPosition;
-		ChangeHeightText();
-	}
-
-	void ChangeHeightText()
-	{
-		_heightText.text = _baseText + (transform.position.y - _heightOffset).ToString();
 	}
 }
