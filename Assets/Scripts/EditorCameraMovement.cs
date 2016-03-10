347b2c5c7d0b66e9ff27ca131d2cf4eef07f85b3
ChessNeo <evonsheika@gmail.com>
2016-03-10 17:30:46 +0100

    More gui + camera up and down movement + hiding bocks system

--- Assets/Scripts/EditorCameraMovement.cs
+++ Assets/Scripts/EditorCameraMovement.cs
@@ -6,6 +6,7 @@ public class EditorCameraMovement : MonoBehaviour
 {
 	[SerializeField] private Text _heightText;
 	[SerializeField] private string _baseText;
+	[SerializeField] private GameObject _mapContainer;
 
 	private float _heightOffset;
 
@@ -13,6 +14,7 @@ public class EditorCameraMovement : MonoBehaviour
 	{
 		_heightOffset = transform.position.y;
 		ChangeHeightText();
+		ChangeHeightView();
 	}
 
 	public void ChangeHeight(float n)
@@ -22,10 +24,22 @@ public class EditorCameraMovement : MonoBehaviour
 		newPosition.y += n;
 		transform.position = newPosition;
 		ChangeHeightText();
+		ChangeHeightView();
 	}
 
 	void ChangeHeightText()
 	{
 		_heightText.text = _baseText + (transform.position.y - _heightOffset).ToString();
+	}
+
+	void ChangeHeightView()
+	{
+		foreach (Transform child in _mapContainer.transform)
+		{
+			if (Mathf.Ceil(child.position.y) != transform.position.y - _heightOffset)
+				child.gameObject.SetActive(false);
+			else
+				child.gameObject.SetActive(true);
+		}
 	}
 }
